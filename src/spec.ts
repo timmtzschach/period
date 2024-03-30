c0f9d0d8740427d1914c9d2156bb7c518c9f0703
ttzschach <contact@timmtzschach.de>
2024-03-30 16:35:13 +0100

    fix(package.json): remove "module" key

--- src/spec.ts
+++ src/spec.ts
@@ -2,11 +2,11 @@ import { Period, RelativeFromStartType } from "./model.js";
 import { getPeriodDates } from "./service.js";
 
 const periods: Period[] = [
-    { from: { timeUnitCount: 1 } },
-    { to: { timeUnitCount: 1 } },
-    { from: { timeUnitCount: 7 } },
-    { to: { timeUnitCount: 7 } },
-    { from: { relativeStartType: RelativeFromStartType.TO }, to: { timeUnitCount: 1 } }
+    { from: { timeUnitCount: 1 } }, // yesterday to now
+    { to: { timeUnitCount: 1 } }, // now to tomorrow
+    { from: { timeUnitCount: 7 } }, // 1 week to now
+    { to: { timeUnitCount: 7 } }, // now to next week
+    { from: { relativeStartType: RelativeFromStartType.TO }, to: { timeUnitCount: 1 } } // now to tomorrow
 ];
 
 for (const period of periods) {
